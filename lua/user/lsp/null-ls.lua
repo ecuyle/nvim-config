7a7b94fb292c16b04db865c027ac0d98eca595c3
Eric Cuyle <ecuyle@gmail.com>
2022-12-03 23:04:38 -0800

    Adds gopls LSP support

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,6 +14,29 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
+    formatting.gofmt,
     -- diagnostics.flake8
 	},
 })
+
+function goimports(timeoutms)
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, "t", true } }
+
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
+
+  local method = "textDocument/codeAction"
+  local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
+  if resp and resp[1] then
+    local result = resp[1].result
+    if result and result[1] then
+      local edit = result[1].edit
+      vim.lsp.util.apply_workspace_edit(edit)
+    end
+  end
+
+  vim.lsp.buf.format { async = true }
+end
+
+vim.cmd "autocmd BufWritePre *.go lua goimports(1000)"
